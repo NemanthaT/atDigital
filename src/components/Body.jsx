32c20e11e8d85ed77bce2c50e3885c9884952495
Tharusha Nemantha <nemanathatharusha@gmail.com>
2025-09-29 23:44:34 +0530

    fix: update asset import paths in Body, Footer, and Header components

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,6 @@
-import Background from "../src/assets/background.png";
-import Computer from "../src/assets/computer.png";
-import Magnify from "../src/assets/magnify.png";
+import Background from "./../src/assets/background.png";
+import Computer from "./../src/assets/computer.png";
+import Magnify from "./../src/assets/magnify.png";
 
 import React, { useState } from "react";
 
